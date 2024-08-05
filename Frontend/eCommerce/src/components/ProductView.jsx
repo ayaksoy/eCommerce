6ee6cc6a79feb2890f22fd0221bd99a5756fa324
ayaksoy <yasir.aksoy@outlook.com>
2024-08-05 14:31:14 +0300

    searchbar

--- Frontend/eCommerce/src/components/ProductView.jsx
+++ Frontend/eCommerce/src/components/ProductView.jsx
@@ -10,6 +10,7 @@ const ProductView = () => {
 	const products = useSelector((state) => state.product.products);
 	const status = useSelector((state) => state.product.status);
 	const [selectedCategoryProducts, setSelectedCategoryProducts] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
 
 	useEffect(() => {
 		if (status === "idle") {
@@ -28,6 +29,19 @@ const ProductView = () => {
 		setSelectedCategoryProducts(products);
 	};
 
+	const handleSearchChange = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const handleSearchSubmit = (event) => {
+		event.preventDefault();
+		// Arama terimine göre ürünleri filtrele
+		const filteredProducts = products.filter((product) =>
+			product.name.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setSelectedCategoryProducts(filteredProducts);
+	};
+
 	return (
 		<div className="product-view">
 			<div className="container">
@@ -37,12 +51,25 @@ const ProductView = () => {
 							<div className="col-lg-12">
 								<div className="row">
 									<div className="col-md-8">
-										<div className="product-search">
-											<input type="email" value="Search" readOnly />
-											<button>
-												<i className="fa fa-search"></i>
-											</button>
-										</div>
+										<form onSubmit={handleSearchSubmit}>
+											<div className="product-search input-group mb-3">
+												<input
+													type="text"
+													className="form-control"
+													placeholder="Search"
+													value={searchTerm}
+													onChange={handleSearchChange}
+												/>
+												<div className="input-group-append">
+													<button
+														className="btn btn-outline-secondary"
+														type="submit"
+													>
+														<i className="fa fa-search"></i>
+													</button>
+												</div>
+											</div>
+										</form>
 									</div>
 								</div>
 							</div>
